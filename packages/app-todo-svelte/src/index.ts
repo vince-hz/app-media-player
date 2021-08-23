c8111ef317c9add8f2dee72631c097357040b277
hyrious <hyrious@outlook.com>
2021-08-23 14:11:27 +0800

    add writable

--- packages/app-todo-svelte/src/index.ts
+++ packages/app-todo-svelte/src/index.ts
@@ -9,7 +9,7 @@ export interface NetlessAppTodoAttributes {
 const NetlessAppTodo: NetlessApp<NetlessAppTodoAttributes> = {
     kind: "Todo",
     setup(context) {
-        const attrs: NetlessAppTodoAttributes = {
+        let attrs: NetlessAppTodoAttributes = {
             current: "",
             list: [],
             ...context.getAttributes(),
@@ -24,10 +24,20 @@ const NetlessAppTodo: NetlessApp<NetlessAppTodoAttributes> = {
         const box = context.getBox();
         box.mountContent(el);
 
+        let writable = context.getIsWritable();
+        context.emitter.on("writableChange", w => {
+            writable = w;
+        });
         app.$on("update", ({ detail }: { detail: NetlessAppTodoAttributes }) => {
-            let a: Record<string, any> = context.getAttributes()!;
-            for (let [k, v] of Object.entries(detail)) {
-                if (a[k] !== v) context.updateAttributes([k], v);
+            if (writable) {
+                let a: Record<string, any> = context.getAttributes()!;
+                for (let [k, v] of Object.entries(detail)) {
+                    if (a[k] !== v) context.updateAttributes([k], v);
+                }
+                attrs = detail;
+            } else {
+                // restore to previous state
+                app.$set(attrs);
             }
         });
         context.emitter.on("attributesUpdate", attrs => {
