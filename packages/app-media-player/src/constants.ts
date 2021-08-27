6a1a49a3cce9a63e6f642d11613ad7fa8f592460
hyrious <hyrious@outlook.com>
2021-08-27 09:37:20 +0800

    update

--- packages/app-media-player/src/constants.ts
+++ packages/app-media-player/src/constants.ts
@@ -1,9 +1,9 @@
 import type { Attributes } from "./types";
 
 /** 插件 ID */
-export const Kind = "MediaPlayer";
+export const Kind = "MediaPlayer2";
 
-export const Version = "0.1.0-alpha.1";
+export const Version = "0.1.0-alpha.5";
 
 export const defaultAttributes: Attributes = {
     src: "",
